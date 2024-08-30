bc498004c775e015eec3e0bf83581db4917f57c7
NPU-Rafael-Leivas <rafael.leivas@npu.com.br>
2024-08-30 16:24:39 -0300

    Adding API connection and sending message using input

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { useState, useEffect } from 'react'
 
 function App() {
   const [body, setBody] = useState('')
+  const [message, setMessage] = useState('')
+  const [senderName, setSenderName] = useState('')
 
   useEffect(() => {
     axios.get('http://localhost:8000/')
       .then(response => {
         const data = response.data
-        console.log(data)
         setBody(data['message'])
       })
       .catch((e) => {
@@ -17,8 +18,54 @@ function App() {
       })
   }, [])
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    axios.post('http://localhost:8000/chat', {
+      message: message,
+      sender_name: senderName  // Inclua o sender_name na requisição
+    })
+      .then(response => {
+        const data = response.data
+        console.log(data)
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    if (name === "message") {
+      setMessage(value)
+    } else if (name === "sender_name") {
+      setSenderName(value)
+    }
+  }
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Mensagem:
+          <input
+            type="text"
+            name="message"
+            value={message}
+            onChange={handleChange}  // Atualiza o estado 'message' quando o usuário digita
+          />
+        </label>
+        <label>
+          Responsável:
+          <input
+            type="text"
+            name="sender_name"
+            value={senderName}
+            onChange={handleChange}  // Atualiza o estado 'senderName' quando o usuário digita
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       <div>
         {body}
       </div>
