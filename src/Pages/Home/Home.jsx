fe1fb9d950d8f9e57a3105ade9f80c643405d9d5
NPU-Rafael-Leivas <rafael.leivas@npu.com.br>
2024-09-16 14:01:34 -0300

    Update front

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -54,7 +54,15 @@ function Home() {
       {senderName === null ? (
         <UsernamePopup onUsernameSet={handleUsernameSet} />
       ) : (
-        <div>
+        <div className={st.body}>
+          <div>
+            <h1>Mande a sua mensagem com o <span>ChatZera</span></h1>
+            <p>It is a long established fact that a reader will be distracted by the  readable content of a page when looking at its layout. The point of  using Lorem Ipsum is that it has a more-or-less normal distribution of  letters</p>
+            <div>
+              <button>Linkedin</button>
+              <button>GitHub</button>
+            </div>
+          </div>
           <Chat user={senderName} />
         </div>
       )}
